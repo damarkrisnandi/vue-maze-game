3c3a528e5368f1997b5706ee430ddf7aa7d26818
damar <damar@gitlab.lab>
2022-02-04 17:03:07 +0700

    fix controller

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -7,9 +7,12 @@
     
     <div class="row">
         <b-button class="col" @click="$emit('direction', {key: 'a'})"><i class="bi-arrow-left-circle" role="img" aria-label="Info" style="font-size: 20px"></i></b-button>
-        <b-button class="col" @click="$emit('direction', {key: 's'})"><i class="bi-arrow-down-circle" role="img" aria-label="Info" style="font-size: 20px"></i></b-button>
         <b-button class="col" @click="$emit('direction', {key: 'd'})"><i class="bi-arrow-right-circle" role="img" aria-label="Info" style="font-size: 20px"></i></b-button>
     </div>
+
+    <b-row style="margin-left: calc(50% - 50vw); width: 100vw;">
+        <b-button class="col" @click="$emit('direction', {key: 's'})"><i class="bi-arrow-down-circle" role="img" aria-label="Info" style="font-size: 20px"></i></b-button>
+    </b-row>
     
 </div>
 </template>
