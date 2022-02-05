2012281e4aeae9761fc61b885f869b27f3252508
damar <damar@gitlab.lab>
2022-02-05 19:27:24 +0700

    update: simplify

--- src/components/GamePlay.vue
+++ src/components/GamePlay.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <button @click="initMap()">Random Map</button> -->
     <div class="controller">
       <Controller @direction="keyDownHandler($event)" />
     </div>
@@ -30,9 +29,9 @@ export default {
       }),
       center: {x: Math.floor((window.innerWidth / 2) / 32) + 1, y: Math.floor((window.innerHeight / 2) / 32) + 1},
       player: null,
-      mazeMapping: [],
       maze: [],
-      mazeVisualizer: null
+      mazeVisualizer: null,
+      unit: 32
     }
   },
   components: {
@@ -49,10 +48,10 @@ export default {
     keyDownHandler(e) {
         // Your handler code here
         const controls = {
-          w: () => { if (this.onTheTop() !== '#') this.mazeVisualizer.y += 32; },
-          a: () => { if (this.onTheLeft() !== '#') this.mazeVisualizer.x += 32; },
-          s: () => { if (this.onTheBottom() !== '#') this.mazeVisualizer.y -= 32; },
-          d: () => { if (this.onTheRight() !== '#') this.mazeVisualizer.x -= 32; }
+          w: () => { if (this.getNeighbor(0, -1) !== '#') this.mazeVisualizer.y += this.unit; },
+          a: () => { if (this.getNeighbor(-1, 0) !== '#') this.mazeVisualizer.x += this.unit; },
+          s: () => { if (this.getNeighbor(0, 1) !== '#') this.mazeVisualizer.y -= this.unit; },
+          d: () => { if (this.getNeighbor(1, 0) !== '#') this.mazeVisualizer.x -= this.unit; }
         };
 
         try {
@@ -60,74 +59,61 @@ export default {
             // update stage 
             this.app.stage.addChild(this.mazeVisualizer);
             this.app.stage.addChild(this.player);
-        } catch (error) {
-          
-        }
+        } catch (error) {}
 
     },
     initializeWorld() {
-      this.app.renderer.backgroundColor = 'black';
-      this.app.renderer.resize(window.innerWidth, window.innerHeight - 64);
-      this.app.renderer.view.style.position = 'absolute';
-      document.body.append(this.app.view);
+        this.app.renderer.backgroundColor = 'black';
+        this.app.renderer.resize(window.innerWidth, window.innerHeight - 64);
+        this.app.renderer.view.style.position = 'absolute';
+        document.body.append(this.app.view);
     },
     initMap() {
-      this.maze = new Maze();
-      this.maze.init();
+        this.maze = new Maze();
+        this.maze.init();
 
-      for (let y = 0; y < this.maze.map.length; y++) {
-        for (let x = 0; x < this.maze.map[y].length; x++) {
-          if (this.maze.map[y][x] === '#') {
-            const data = {x: 13, y: 11, nowx: x + 1, nowy: y + 1};
-            this.mazeMapping.push(data);
-          } else if (this.maze.map[y][x] === '.') {
-            const data2 = {x: 21, y: 1, nowx: x + 1, nowy: y + 1};
-            this.mazeMapping.push(data2);
-          } else {
-            const animateGold = animator(item.gold, {x: 32, y: 32})
-            animateGold.animationSpeed = 0.2
-            animateGold.play();
-            const data3 = {stack: true, animation: animateGold, x: 21, y: 1, nowx: x + 1, nowy: y + 1};
-            this.mazeMapping.push(data3);
-          }
+        const mazeMapping = [];
+        for (let y = 0; y < this.maze.map.length; y++) {
+            for (let x = 0; x < this.maze.map[y].length; x++) {
+                if (this.maze.map[y][x] === '#') {
+                    const data = {x: 13, y: 11, nowx: x + 1, nowy: y + 1};
+                    mazeMapping.push(data);
+                } else if (this.maze.map[y][x] === '.') {
+                    const data2 = {x: 21, y: 1, nowx: x + 1, nowy: y + 1};
+                    mazeMapping.push(data2);
+                } else {
+                    const animateGold = animator(item.gold, {x: this.unit, y: this.unit})
+                    animateGold.animationSpeed = 0.2
+                    animateGold.play();
+                    const data3 = {stack: true, animation: animateGold, x: 21, y: 1, nowx: x + 1, nowy: y + 1};
+                    mazeMapping.push(data3);
+                }
+            }
         }
-      }
-      
-      this.mazeVisualizer = blockBuilder(this.mazeMapping, {x: 1, y: 1});
-      this.app.stage.addChild(this.mazeVisualizer);
+        
+        this.mazeVisualizer = blockBuilder(mazeMapping, {x: 1, y: 1});
+        this.app.stage.addChild(this.mazeVisualizer);
     },
     initPlayer() {
-      this.player = blockBuilder(allObject.player, this.center);
-      this.app.stage.addChild(this.player);
+        this.player = blockBuilder(allObject.player, this.center);
+        this.app.stage.addChild(this.player);
     },
-    onTheRight() {
-      return this.maze.map[
-        ((this.player.y)/ 32) + (this.center.y - 1) - (this.mazeVisualizer.y / 32)
-      ][
-        ((this.player.x + 32)/ 32) + (this.center.x - 1) - (this.mazeVisualizer.x / 32)
-      ]
-    },
-    onTheLeft() {
-      return this.maze.map[
-        ((this.player.y)/ 32) + (this.center.y - 1) - (this.mazeVisualizer.y / 32)
-      ][
-        ((this.player.x - 32)/ 32) + (this.center.x - 1) - (this.mazeVisualizer.x / 32)
-      ]
-    },
-    onTheTop() {
-      return this.maze.map[
-        ((this.player.y - 32)/ 32) + (this.center.y - 1) - (this.mazeVisualizer.y / 32)
-      ][
-        ((this.player.x)/ 32) + (this.center.x - 1) - (this.mazeVisualizer.x / 32)
-      ]
-    },
-    onTheBottom() {
-      return this.maze.map[
-        ((this.player.y + 32)/ 32) + (this.center.y - 1) - (this.mazeVisualizer.y / 32)
-      ][
-        ((this.player.x)/ 32) + (this.center.x - 1) - (this.mazeVisualizer.x / 32)
-      ]
-    }
+    /**
+     * @param x horizontal neighbor unit
+     * @param y vertical neighbor unit
+     * @returns object pada posisi (x, y) dari sekitar player.
+     * - (1, 0) sebelah kanan
+     * - (-1, 0) sebelah kiri
+     * - (0, -1) di atas
+     * - (0, 1) di bawah
+     */
+    getNeighbor(x, y) {
+            const pos = {
+                y: ((this.player.y + (this.unit*y))/ this.unit) + (this.center.y - 1) - (this.mazeVisualizer.y / this.unit),
+                x: ((this.player.x + (this.unit*x))/ this.unit) + (this.center.x - 1) - (this.mazeVisualizer.x / this.unit)
+            };
+            return this.maze.map[pos.y][pos.x];
+        }
   }
 }
 </script>
